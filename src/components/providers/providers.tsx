77616cc51ac0cf031a054a147248e4c17c90ce1e
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-10-23 06:03:25 +0700

    refactor: Remove ThemeProvider from Providers component

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,12 +1,7 @@
 'use client';
 
 import React from 'react';
-import { ThemeProvider } from './theme-providers';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <ThemeProvider defaultTheme='system'>{children}</ThemeProvider>
-    </>
-  );
+  return <>{children}</>;
 }
